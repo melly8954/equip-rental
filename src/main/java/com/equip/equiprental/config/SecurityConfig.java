d86070d0c49df1ba13d4f570daefcaa0d21740b2
haily <49448330+melly8954@users.noreply.github.com>
2025-09-03 06:59:50 +0900

    feat: 시큐리티 Config 및 암호화 Config 등록 (250903)
    - 스프링 시큐리티 필터체인을 활용할 Config 등록
    - 패스워드 암호화를 위한 Encoder Config 등록

---
+++ src/main/java/com/equip/equiprental/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.equip.equiprental.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/api/v1/members")
+                        .permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
