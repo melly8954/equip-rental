8be546618909068aadf71a56824021dbe7870284
haily <49448330+melly8954@users.noreply.github.com>
2025-09-03 06:55:25 +0900

    feat: API 공통 응답 포맷 구현 (250903)

---
+++ src/main/java/com/equip/equiprental/common/dto/ResponseDto.java
@@ -0,0 +1,17 @@
+package com.equip.equiprental.common.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseDto<T> {
+    private String id;
+    private int code;
+    private String errorCode;
+    private String message;
+    private T data;
+}
+
