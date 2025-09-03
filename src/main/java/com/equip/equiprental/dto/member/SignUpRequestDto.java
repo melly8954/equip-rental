217ad501bbc761490b65db3d8860906a2ee774b4
haily <49448330+melly8954@users.noreply.github.com>
2025-09-03 16:08:54 +0900

    feat: dto @Setter 추가 (250903)

--- src/main/java/com/equip/equiprental/dto/member/SignUpRequestDto.java
+++ src/main/java/com/equip/equiprental/dto/member/SignUpRequestDto.java
@@ -3,8 +3,10 @@ package com.equip.equiprental.dto.member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @Builder
 public class SignUpRequestDto {
