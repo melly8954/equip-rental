ac99763dd8f0ea3761b78b2caeb8272d6518a0bb
haily <49448330+melly8954@users.noreply.github.com>
2025-09-03 19:26:15 +0900

    feat: Spring Security 커스텀 인증 로직 구현 (250903)
    - AuthenticationProvider 구현체 CustomAuthenticationProvider  추가
    - 시큐리티에 사용할 AuthenticationManager 및  CustomAuthenticationProvider 빈 등록

--- src/main/java/com/equip/equiprental/config/SecurityConfig.java
+++ src/main/java/com/equip/equiprental/config/SecurityConfig.java
@@ -1,19 +1,29 @@
 package com.equip.equiprental.config;
 
+import com.equip.equiprental.common.auth.CustomAuthenticationProvider;
+import com.equip.equiprental.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -24,8 +34,18 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/js/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/", "/signup").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/api/v1/members").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/members", "/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider customAuthenticationProvider() {
+        return new CustomAuthenticationProvider(memberRepository, passwordEncoder);
     }
 }
