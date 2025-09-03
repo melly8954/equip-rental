828e18f891629e0eff5101bee2f72bd88b60c7ea
haily <49448330+melly8954@users.noreply.github.com>
2025-09-03 22:22:28 +0900

    feat: 로그인 API 프론트 구현 및 성공 시 role 에 의한 redirect 처리 (250903)

--- src/main/java/com/equip/equiprental/common/controller/AllViewController.java
+++ src/main/java/com/equip/equiprental/common/controller/AllViewController.java
@@ -1,8 +1,10 @@
 package com.equip.equiprental.common.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class AllViewController {
@@ -18,6 +20,18 @@ public class AllViewController {
 
     @GetMapping("/equipment")
     public String equipment() {
-        return "equipment/equipment";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getAuthorities() != null) {
+            boolean isAdminOrManager  = authentication.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .anyMatch(role -> role.equals("ROLE_ADMIN") || role.equals("ROLE_MANAGER"));
+
+            if (isAdminOrManager ) {
+                return "equipment/adminEquipment";  // 관리자용 페이지
+            }
+        }
+
+        return "equipment/userEquipment";  // 일반 사용자 페이지
     }
 }
