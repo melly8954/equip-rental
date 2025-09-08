57e5c80bc2fe3b4b8dff184947affb774384f7fa
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 15:23:00 +0900

    feat: 장비 등록 API 구현 완료 (250908)

--- src/main/java/com/equip/equiprental/equipment/service/EquipmentServiceImpl.java
+++ src/main/java/com/equip/equiprental/equipment/service/EquipmentServiceImpl.java
@@ -1,10 +1,16 @@
 package com.equip.equiprental.equipment.service;
 
 
+import com.equip.equiprental.common.exception.CustomException;
+import com.equip.equiprental.common.exception.ErrorType;
 import com.equip.equiprental.equipment.domain.Equipment;
+import com.equip.equiprental.equipment.domain.EquipmentItem;
+import com.equip.equiprental.equipment.domain.EquipmentStatus;
 import com.equip.equiprental.equipment.dto.EquipmentRegisterRequest;
 import com.equip.equiprental.equipment.dto.EquipmentRegisterResponse;
+import com.equip.equiprental.equipment.repository.EquipmentItemRepository;
 import com.equip.equiprental.equipment.repository.EquipmentRepository;
+import com.equip.equiprental.equipment.util.ModelCodeGenerator;
 import com.equip.equiprental.filestorage.domain.FileMeta;
 import com.equip.equiprental.filestorage.repository.FileRepository;
 import com.equip.equiprental.filestorage.service.FileService;
@@ -15,21 +21,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class EquipmentServiceImpl implements EquipmentService {
     private final EquipmentRepository equipmentRepository;
+    private final EquipmentItemRepository equipmentItemRepository;
+    private final ModelCodeGenerator modelCodeGenerator;
     private final FileRepository fileRepository;
     private final FileService fileService;
 
     @Override
     @Transactional
     public EquipmentRegisterResponse register(EquipmentRegisterRequest dto, List<MultipartFile> files) {
+        // 모델이 이미 존재하면 예외 던지기
+        if (equipmentRepository.findByModel(dto.getModel()).isPresent()) {
+            throw new CustomException(ErrorType.EXIST_EQUIPMENT_MODEL_CODE);
+        }
+        // 없으면 새 장비 생성
+        String modelCode = modelCodeGenerator.generate();
+
         Equipment equipment = Equipment.builder()
                 .category(dto.getCategoryEnum())
                 .subCategory(dto.getSubCategory())
                 .model(dto.getModel())
+                .modelCode(modelCode)
                 .stock(dto.getStock())
                 .build();
         equipmentRepository.save(equipment);
@@ -61,6 +78,25 @@ public class EquipmentServiceImpl implements EquipmentService {
             fileRepository.saveAll(savedFiles);
         }
 
+        // EquipmentItem 생성 (stock 수량만큼)
+        long baseSequence = equipmentRepository.countByModel(equipment.getModel())-1; // 방금 생성한 장비를 제외하고 기존 수량만 가져오기
+        List<EquipmentItem> items = new ArrayList<>();
+
+        for (int i = 0; i < equipment.getStock(); i++) {
+            long sequence = baseSequence + i + 1; // 반복마다 시퀀스 증가
+            String serialNumber = generateSerialNumber(equipment.getModelCode(), sequence);
+
+            EquipmentItem item = EquipmentItem.builder()
+                    .equipment(equipment)
+                    .serialNumber(serialNumber)
+                    .status(EquipmentStatus.AVAILABLE) // 초기 상태
+                    .build();
+
+            items.add(item);
+        }
+        equipmentItemRepository.saveAll(items);
+
+        // 응답 객체 생성
         List<EquipmentRegisterResponse.EquipmentImage> lists = new ArrayList<>();
         for (FileMeta meta : savedFiles) {
             lists.add(EquipmentRegisterResponse.EquipmentImage.builder()
@@ -83,5 +119,13 @@ public class EquipmentServiceImpl implements EquipmentService {
                 .createdAt(equipment.getCreatedAt())
                 .updatedAt(equipment.getUpdatedAt())
                 .build();
+    }
+
+    private String generateSerialNumber(String modelCode, long sequence) {
+        // 랜덤 4자리 숫자 생성
+        int random4 = new Random().nextInt(10000); // 0~9999
+        String random4Str = String.format("%04d", random4); // 4자리 고정, 앞에 0 채우기
+
+        return modelCode + "-" + sequence + "-" + random4Str;
     }
 }
