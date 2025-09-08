70ebbc2f940c966a6080d4195162847222e34abb
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 20:55:45 +0900

    feat: 장비 조회 응답용 Response 객체 EquipmentDto  추가 (250908)

---
+++ src/main/java/com/equip/equiprental/equipment/dto/EquipmentDto.java
@@ -0,0 +1,14 @@
+package com.equip.equiprental.equipment.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class EquipmentDto {
+    private Long equipmentId;
+    private String category;
+    private String subCategory;
+    private String model;
+    private Integer stock;
+}
