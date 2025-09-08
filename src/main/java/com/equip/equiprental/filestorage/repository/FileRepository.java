4863e90279812a349a923ad0cb931d2453b19c55
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 21:22:22 +0900

    feat: 파일 url 조회를 위한 findUrlsByEquipmentId 쿼리 추가

--- src/main/java/com/equip/equiprental/filestorage/repository/FileRepository.java
+++ src/main/java/com/equip/equiprental/filestorage/repository/FileRepository.java
@@ -2,6 +2,14 @@ package com.equip.equiprental.filestorage.repository;
 
 import com.equip.equiprental.filestorage.domain.FileMeta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FileRepository extends JpaRepository<FileMeta, Long> {
+    @Query("SELECT f.filePath FROM FileMeta f " +
+            "WHERE f.relatedId = :equipmentId " +
+            "ORDER BY f.createdAt ASC")
+    List<String> findUrlsByEquipmentId(@Param("equipmentId") Long equipmentId);
 }
