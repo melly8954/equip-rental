b68e689afef936ac36ca9fb8bb50b9e1dc3a9ffb
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 12:53:18 +0900

    feat: 관리자 장비등록 API ((250908))
    - 파일 첨부 구현 완료
    - 등록 수량에 따른 item 자동 추가 미구현

--- src/main/java/com/equip/equiprental/equipment/dto/EquipmentRegisterResponse.java
+++ src/main/java/com/equip/equiprental/equipment/dto/EquipmentRegisterResponse.java
@@ -4,8 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @Builder
 public class EquipmentRegisterResponse {
+    private Long equipmentId;
+    private String category;
+    private String subCategory;
+    private String model;
+    private int stock;
+    private List<EquipmentImage> images;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class EquipmentImage {
+        private Long fileId;
+        private String originalName;
+        private String url;
+        private int fileOrder;
+        private String fileType;
+        private long fileSize;
+    }
 }
--- src/main/java/com/equip/equiprental/equipment/service/EquipmentServiceImpl.java
+++ src/main/java/com/equip/equiprental/equipment/service/EquipmentServiceImpl.java
@@ -1,20 +1,27 @@
 package com.equip.equiprental.equipment.service;
 
+
 import com.equip.equiprental.equipment.domain.Equipment;
 import com.equip.equiprental.equipment.dto.EquipmentRegisterRequest;
 import com.equip.equiprental.equipment.dto.EquipmentRegisterResponse;
 import com.equip.equiprental.equipment.repository.EquipmentRepository;
+import com.equip.equiprental.filestorage.domain.FileMeta;
+import com.equip.equiprental.filestorage.repository.FileRepository;
+import com.equip.equiprental.filestorage.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EquipmentServiceImpl implements EquipmentService {
     private final EquipmentRepository equipmentRepository;
+    private final FileRepository fileRepository;
+    private final FileService fileService;
 
     @Override
     @Transactional
@@ -26,7 +33,55 @@ public class EquipmentServiceImpl implements EquipmentService {
                 .stock(dto.getStock())
                 .build();
         equipmentRepository.save(equipment);
-        
-        return null;
+
+        List<FileMeta> savedFiles = new ArrayList<>();
+
+        if(files != null && !files.isEmpty()) {
+            int fileOrder = 0;
+            List<String> fileUrls = fileService.saveFiles(files, "equipment");
+
+            for (int i = 0; i < files.size(); i++) {
+                MultipartFile file = files.get(i);
+                String url = fileUrls.get(i); // fileService에서 생성한 접근 URL
+
+                FileMeta meta = FileMeta.builder()
+                        .relatedType("equipment")
+                        .relatedId(equipment.getEquipmentId())
+                        .originalName(file.getOriginalFilename())
+                        .uniqueName(url.substring(url.lastIndexOf("/") + 1)) // URL 에서 uniqueName 추출
+                        .fileOrder(fileOrder++)
+                        .fileType(file.getContentType())
+                        .filePath(url) // 접근 URL
+                        .fileSize(file.getSize())
+                        .build();
+
+                savedFiles.add(meta);
+            }
+
+            fileRepository.saveAll(savedFiles);
+        }
+
+        List<EquipmentRegisterResponse.EquipmentImage> lists = new ArrayList<>();
+        for (FileMeta meta : savedFiles) {
+            lists.add(EquipmentRegisterResponse.EquipmentImage.builder()
+                    .fileId(meta.getFileId())
+                    .originalName(meta.getOriginalName())
+                    .url(meta.getFilePath())
+                    .fileOrder(meta.getFileOrder())
+                    .fileType(meta.getFileType())
+                    .fileSize(meta.getFileSize())
+                    .build());
+        }
+
+        return EquipmentRegisterResponse.builder()
+                .equipmentId(equipment.getEquipmentId())
+                .category(equipment.getCategory().name())
+                .subCategory(equipment.getSubCategory())
+                .model(equipment.getModel())
+                .stock(equipment.getStock())
+                .images(lists)
+                .createdAt(equipment.getCreatedAt())
+                .updatedAt(equipment.getUpdatedAt())
+                .build();
     }
 }
