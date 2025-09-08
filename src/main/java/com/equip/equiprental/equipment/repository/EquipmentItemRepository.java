912440fa09a1cffb89f0739431fb4731da6f070d
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 15:19:17 +0900

    feat: existsBySerialNumber 쿼리 추가 (250908)

--- src/main/java/com/equip/equiprental/equipment/repository/EquipmentItemRepository.java
+++ src/main/java/com/equip/equiprental/equipment/repository/EquipmentItemRepository.java
@@ -4,4 +4,5 @@ import com.equip.equiprental.equipment.domain.EquipmentItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EquipmentItemRepository extends JpaRepository<EquipmentItem, Long> {
+    boolean existsBySerialNumber(String serialNumber); // 랜덤 중복 체크용
 }
