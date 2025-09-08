2744a8124ef79ae3cca2018fd32443816e1b50d1
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 12:47:38 +0900

    feat: MaxUploadSizeExceededException 추가 (250908)

--- src/main/java/com/equip/equiprental/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/equip/equiprental/common/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 @Slf4j
 @RestControllerAdvice
@@ -16,6 +17,23 @@ public class GlobalExceptionHandler implements ResponseController {
         String traceId = RequestTraceIdInterceptor.getTraceId();
         ErrorType errorType = e.getErrorType();
         log.error("TraceId: {}, 비즈니스 로직 예외 발생 - Code: {}, Message: {}",
+                traceId, errorType.getErrorCode(), errorType.getMessage());
+
+        return makeResponseEntity(
+                traceId,
+                errorType.getStatus(),
+                errorType.getErrorCode(),
+                errorType.getMessage(),
+                null
+        );
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ResponseDto<Void>> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException e) {
+        String traceId = RequestTraceIdInterceptor.getTraceId();
+
+        ErrorType errorType = ErrorType.FILE_SIZE_EXCEEDED;
+        log.error("TraceId: {}, 파일 첨부 업로드 크기 예외 발생 - Code: {}, Message: {}",
                 traceId, errorType.getErrorCode(), errorType.getMessage());
 
         return makeResponseEntity(
