b81baa064ab76ed119b394f2823f0e4b9cf4ce16
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 10:52:28 +0900

    feat: 장비 등록 API 요청 / 응답 DTO 구현 (250908)

---
+++ src/main/java/com/equip/equiprental/equipment/dto/EquipmentRegisterResponse.java
@@ -0,0 +1,11 @@
+package com.equip.equiprental.equipment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class EquipmentRegisterResponse {
+}
