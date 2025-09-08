65494cdeb02aea4ad2ff5695589d6567fe98affb
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 18:36:32 +0900

    feat: 파일 미첨부 시 발생하는 MissingServletRequestPartException 전역 처리 추가 (250908)

--- src/main/java/com/equip/equiprental/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/equip/equiprental/common/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 @Slf4j
 @RestControllerAdvice
@@ -34,6 +35,23 @@ public class GlobalExceptionHandler implements ResponseController {
 
         ErrorType errorType = ErrorType.FILE_SIZE_EXCEEDED;
         log.error("TraceId: {}, 파일 첨부 업로드 크기 예외 발생 - Code: {}, Message: {}",
+                traceId, errorType.getErrorCode(), errorType.getMessage());
+
+        return makeResponseEntity(
+                traceId,
+                errorType.getStatus(),
+                errorType.getErrorCode(),
+                errorType.getMessage(),
+                null
+        );
+    }
+
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    public ResponseEntity<ResponseDto<Void>> handleMissingPart(MissingServletRequestPartException ex) {
+        String traceId = RequestTraceIdInterceptor.getTraceId();
+
+        ErrorType errorType = ErrorType.FILE_REQUIRED;
+        log.error("TraceId: {}, 파일 미첨부 예외 발생 - Code: {}, Message: {}",
                 traceId, errorType.getErrorCode(), errorType.getMessage());
 
         return makeResponseEntity(
