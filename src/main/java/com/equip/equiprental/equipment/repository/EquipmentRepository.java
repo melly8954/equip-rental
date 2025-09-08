850de08b8fe2da64a81653e74baed38d8a1864bf
haily <49448330+melly8954@users.noreply.github.com>
2025-09-08 15:21:08 +0900

    feat: EquipmentRepository 구현 (250908)
    - 모델을 통한 장비 조회
    - 모델 코드를 통한 해당 장비 개수 조회

--- src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
+++ src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
@@ -3,5 +3,10 @@ package com.equip.equiprental.equipment.repository;
 import com.equip.equiprental.equipment.domain.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+    Optional<Equipment> findByModel(String model);
+
+    long countByModel(String modelCode);
 }
