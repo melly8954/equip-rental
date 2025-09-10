4faa373a0623edd6d75e7d988557f7d99e253b18
haily <49448330+melly8954@users.noreply.github.com>
2025-09-10 22:46:40 +0900

    feat: 장비 아이템 상태 변경 요청 Dto 추가

--- src/main/java/com/equip/equiprental/equipment/dto/UpdateItemStatusDto.java
+++ src/main/java/com/equip/equiprental/equipment/dto/UpdateItemStatusDto.java
@@ -1,4 +1,27 @@
 package com.equip.equiprental.equipment.dto;
 
+import com.equip.equiprental.common.exception.CustomException;
+import com.equip.equiprental.common.exception.ErrorType;
+import com.equip.equiprental.equipment.domain.EquipmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
 public class UpdateItemStatusDto {
+    private Long equipmentItemId;
+    private String newStatus;
+
+    public EquipmentStatus getEquipmentItemStatusEnum(){
+        if(this.newStatus == null){
+            throw new CustomException(ErrorType.INVALID_STATUS_REQUEST);
+        }
+        try{
+            return EquipmentStatus.valueOf(this.newStatus);
+        }catch(IllegalArgumentException e){
+            throw new CustomException(ErrorType.INVALID_STATUS_REQUEST);
+        }
+    }
 }
