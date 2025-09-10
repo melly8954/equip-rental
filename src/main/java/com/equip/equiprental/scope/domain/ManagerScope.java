76bfc8e916cbd37271baff25f224379bf59d757b
haily <49448330+melly8954@users.noreply.github.com>
2025-09-10 14:34:34 +0900

    feat: 매니저 스코프 도메인 추가 (복합 PK 적용)

---
+++ src/main/java/com/equip/equiprental/scope/domain/ManagerScope.java
@@ -0,0 +1,25 @@
+package com.equip.equiprental.scope.domain;
+
+import com.equip.equiprental.equipment.domain.EquipmentCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="manager_scope_tbl")
+@IdClass(ManagerScopeId.class)
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ManagerScope {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="manager_id")
+    private Long managerId;
+
+    @Id
+    private String  category;
+}
