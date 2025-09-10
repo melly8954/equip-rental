e8c7440a53287e271fe810e72f556922d253724b
haily <49448330+melly8954@users.noreply.github.com>
2025-09-10 21:48:30 +0900

    feat: 시퀀스 값 중 max 값을 조회할 메서드 쿼리 추가

--- src/main/java/com/equip/equiprental/equipment/repository/EquipmentItemRepository.java
+++ src/main/java/com/equip/equiprental/equipment/repository/EquipmentItemRepository.java
@@ -3,6 +3,12 @@ package com.equip.equiprental.equipment.repository;
 import com.equip.equiprental.equipment.domain.EquipmentItem;
 import com.equip.equiprental.equipment.repository.dsl.EquipmentItemQRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface EquipmentItemRepository extends JpaRepository<EquipmentItem, Long>, EquipmentItemQRepo {
+    @Query("SELECT MAX(e.sequence) FROM EquipmentItem e WHERE e.equipment.model = :model")
+    Optional<Long> findMaxSequenceByModel(@Param("model") String model);
 }
