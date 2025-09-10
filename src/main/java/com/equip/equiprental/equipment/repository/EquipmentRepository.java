66b80134d87c8b6d17646dc5467e267227af1622
haily <49448330+melly8954@users.noreply.github.com>
2025-09-10 21:49:03 +0900

    feat: 장비 id 기반 장비 조회 쿼리 메서드 추가

--- src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
+++ src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long>, EquipmentQRepo {
+    Optional<Equipment> findByEquipmentId(Long equipmentId);
     Optional<Equipment> findByModel(String model);
     long countByModel(String modelCode);
 
