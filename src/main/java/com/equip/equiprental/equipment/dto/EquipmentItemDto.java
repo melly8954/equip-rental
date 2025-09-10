420d40f2ff99683559b05a5b789d9d2ee57f2e6b
haily <49448330+melly8954@users.noreply.github.com>
2025-09-10 10:41:17 +0900

    fix: 추가했던 EquipmentDto equipment 필드 제거

--- src/main/java/com/equip/equiprental/equipment/dto/EquipmentItemDto.java
+++ src/main/java/com/equip/equiprental/equipment/dto/EquipmentItemDto.java
@@ -10,7 +10,6 @@ import lombok.Getter;
 @Builder
 public class EquipmentItemDto {
     private Long equipmentItemId;
-    private EquipmentDto equipment;
     private String serialNumber;
     private EquipmentStatus status;
 }
