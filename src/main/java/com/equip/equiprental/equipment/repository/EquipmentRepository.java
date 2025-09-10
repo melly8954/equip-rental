b3374fa6bbc63151c84b8b4cfb5d68a689d022ac
haily <49448330+melly8954@users.noreply.github.com>
2025-09-10 14:33:39 +0900

    feat: 장비 id 로 카테고리 조회 쿼리 메서드 추가

--- src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
+++ src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
@@ -1,12 +1,18 @@
 package com.equip.equiprental.equipment.repository;
 
 import com.equip.equiprental.equipment.domain.Equipment;
+import com.equip.equiprental.equipment.domain.EquipmentCategory;
 import com.equip.equiprental.equipment.repository.dsl.EquipmentQRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long>, EquipmentQRepo {
     Optional<Equipment> findByModel(String model);
     long countByModel(String modelCode);
+
+    @Query("SELECT e.category FROM Equipment e WHERE e.equipmentId = :equipmentId")
+    EquipmentCategory findCategoryByEquipmentId(@Param("equipmentId") Long equipmentId);
 }
