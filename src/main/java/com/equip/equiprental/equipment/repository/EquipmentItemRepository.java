8837b67de0df89bc91b46b27c03040643e725b17
haily <49448330+melly8954@users.noreply.github.com>
2025-09-11 12:37:08 +0900

    fix: 불필요한 코드 제거
    - findByIdWithEquipment 쿼리 메서드 제거

--- src/main/java/com/equip/equiprental/equipment/repository/EquipmentItemRepository.java
+++ src/main/java/com/equip/equiprental/equipment/repository/EquipmentItemRepository.java
@@ -9,9 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface EquipmentItemRepository extends JpaRepository<EquipmentItem, Long>, EquipmentItemQRepo {
-    @Query("SELECT i FROM EquipmentItem i JOIN FETCH i.equipment WHERE i.equipmentItemId = :id")
-    Optional<EquipmentItem> findByIdWithEquipment(@Param("id") Long id);
-
     @Query("SELECT MAX(e.sequence) FROM EquipmentItem e WHERE e.equipment.model = :model")
     Optional<Long> findMaxSequenceByModel(@Param("model") String model);
 }
