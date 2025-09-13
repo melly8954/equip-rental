327eba5c5f4bf799a97b765e30320672bf8cd3d7
haily <49448330+melly8954@users.noreply.github.com>
2025-09-13 15:34:29 +0900

    chore: findUrlsByEquipmentId 쿼리 메서드 주석 추가

--- src/main/java/com/equip/equiprental/filestorage/repository/FileRepository.java
+++ src/main/java/com/equip/equiprental/filestorage/repository/FileRepository.java
@@ -11,5 +11,6 @@ public interface FileRepository extends JpaRepository<FileMeta, Long> {
     @Query("SELECT f.filePath FROM FileMeta f " +
             "WHERE f.relatedId = :equipmentId " +
             "ORDER BY f.createdAt ASC")
+    // 파일 url 리스트 (ASC 조회로 가장 앞 대표 이미지 url 얻을 수 있음)
     List<String> findUrlsByEquipmentId(@Param("equipmentId") Long equipmentId);
 }
