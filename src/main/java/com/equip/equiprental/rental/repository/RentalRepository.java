364f8acbf3275d04e075204ec925dc5b949fcd1a
haily <49448330+melly8954@users.noreply.github.com>
2025-09-15 01:31:56 +0900

    feat: extends RentalQRepo 추가

--- src/main/java/com/equip/equiprental/rental/repository/RentalRepository.java
+++ src/main/java/com/equip/equiprental/rental/repository/RentalRepository.java
@@ -1,7 +1,8 @@
 package com.equip.equiprental.rental.repository;
 
 import com.equip.equiprental.rental.domain.Rental;
+import com.equip.equiprental.rental.repository.dsl.RentalQRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RentalRepository extends JpaRepository<Rental, Integer> {
+public interface RentalRepository extends JpaRepository<Rental, Integer>, RentalQRepo {
 }
