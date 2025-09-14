50b71de3e27c2a71eb71385846428aac9a8a59c1
haily <49448330+melly8954@users.noreply.github.com>
2025-09-15 01:28:54 +0900

    feat: 관리자 장비 대여 신청내역 조회 API 추가

--- src/main/java/com/equip/equiprental/rental/controller/RentalController.java
+++ src/main/java/com/equip/equiprental/rental/controller/RentalController.java
@@ -2,8 +2,11 @@ package com.equip.equiprental.rental.controller;
 
 import com.equip.equiprental.auth.security.PrincipalDetails;
 import com.equip.equiprental.common.controller.ResponseController;
+import com.equip.equiprental.common.dto.PageResponseDto;
 import com.equip.equiprental.common.dto.ResponseDto;
+import com.equip.equiprental.common.dto.SearchParamDto;
 import com.equip.equiprental.common.interceptor.RequestTraceIdInterceptor;
+import com.equip.equiprental.rental.dto.AdminRentalDto;
 import com.equip.equiprental.rental.dto.RentalRequestDto;
 import com.equip.equiprental.rental.dto.RentalResponseDto;
 import com.equip.equiprental.rental.service.RentalService;
@@ -11,11 +14,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -36,5 +37,15 @@ public class RentalController implements ResponseController {
         RentalResponseDto result = rentalService.requestRental(dto, memberId);
 
         return makeResponseEntity(traceId, HttpStatus.OK, null, "장비 대여 신청 성공", result);
+    }
+
+    @GetMapping("")
+    @PreAuthorize("hasRole('ADMIN') or (hasRole('MANAGER'))")
+    public ResponseEntity<ResponseDto<PageResponseDto<AdminRentalDto>>> getAdminRentalList(@ModelAttribute SearchParamDto paramDto){
+        String traceId = RequestTraceIdInterceptor.getTraceId();
+        log.info("관리자 장비 대여 신청내역 조회 요청 API] TraceId={}", traceId);
+
+        PageResponseDto<AdminRentalDto> result = rentalService.getAdminRentalList(paramDto);
+        return makeResponseEntity(traceId, HttpStatus.OK, null, "관리자 장비 대여 신청내역 조히 성공", result);
     }
 }
