e8cbface7eacf3be164b2a60f7d7a1ca9fb6fb75
haily <49448330+melly8954@users.noreply.github.com>
2025-09-15 01:34:55 +0900

    chore: 불필요한 코드 제거

--- src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
+++ src/main/java/com/equip/equiprental/equipment/repository/EquipmentRepository.java
@@ -3,14 +3,13 @@ package com.equip.equiprental.equipment.repository;
 import com.equip.equiprental.equipment.domain.Equipment;
 import com.equip.equiprental.equipment.domain.EquipmentCategory;
 import com.equip.equiprental.equipment.repository.dsl.EquipmentQRepo;
-import com.equip.equiprental.rental.repository.dsl.RentalQRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface EquipmentRepository extends JpaRepository<Equipment, Long>, EquipmentQRepo, RentalQRepo {
+public interface EquipmentRepository extends JpaRepository<Equipment, Long>, EquipmentQRepo {
     Optional<Equipment> findByEquipmentId(Long equipmentId);
     Optional<Equipment> findByModel(String model);
 
