11cace738e37ff60669a64af9b7ff18066749249
haily <49448330+melly8954@users.noreply.github.com>
2025-09-18 19:30:45 +0900

    feat: 대여 연장 기능 구현

--- src/main/java/com/equip/equiprental/rental/service/RentalItemServiceImpl.java
+++ src/main/java/com/equip/equiprental/rental/service/RentalItemServiceImpl.java
@@ -2,13 +2,18 @@ package com.equip.equiprental.rental.service;
 
 import com.equip.equiprental.common.dto.PageResponseDto;
 import com.equip.equiprental.common.dto.SearchParamDto;
+import com.equip.equiprental.common.exception.CustomException;
+import com.equip.equiprental.common.exception.ErrorType;
+import com.equip.equiprental.rental.domain.RentalItem;
 import com.equip.equiprental.rental.dto.AdminRentalItemDto;
+import com.equip.equiprental.rental.dto.ExtendRequestDto;
 import com.equip.equiprental.rental.dto.UserRentalItemDto;
 import com.equip.equiprental.rental.repository.RentalItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
@@ -19,6 +24,7 @@ public class RentalItemServiceImpl implements RentalItemService{
     private final RentalItemRepository rentalItemRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public PageResponseDto<AdminRentalItemDto> getAdminRentalItemLists(SearchParamDto paramDto) {
         Pageable pageable = paramDto.getPageable();
 
@@ -46,6 +52,7 @@ public class RentalItemServiceImpl implements RentalItemService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public PageResponseDto<UserRentalItemDto> getUserRentalItemLists(SearchParamDto paramDto, Long memberId) {
         Pageable pageable = paramDto.getPageable();
 
@@ -70,5 +77,17 @@ public class RentalItemServiceImpl implements RentalItemService{
                 .last(dtosPage.isLast())
                 .empty(dtosPage.isEmpty())
                 .build();
+    }
+
+    @Override
+    @Transactional
+    public void extendRentalItem(Long rentalItem, ExtendRequestDto dto) {
+        RentalItem item = rentalItemRepository.findById(rentalItem)
+                .orElseThrow(() -> new CustomException(ErrorType.RENTAL_NOT_FOUND));
+
+        LocalDate extendedEndDate = item.getEndDate().plusDays(dto.getDays());
+
+        // 엔티티 수정 메서드 호출
+        item.extend(extendedEndDate);
     }
 }
