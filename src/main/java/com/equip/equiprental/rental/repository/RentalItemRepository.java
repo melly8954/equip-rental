38bad14d09bc8bacbc3a701f14032210203e67da
haily <49448330+melly8954@users.noreply.github.com>
2025-09-21 01:51:02 +0900

    feat: 대여 장비 상태 overdue 변경 쿼리 메서드

--- src/main/java/com/equip/equiprental/rental/repository/RentalItemRepository.java
+++ src/main/java/com/equip/equiprental/rental/repository/RentalItemRepository.java
@@ -3,7 +3,14 @@ package com.equip.equiprental.rental.repository;
 import com.equip.equiprental.rental.domain.RentalItem;
 import com.equip.equiprental.rental.repository.dsl.RentalItemQRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RentalItemRepository extends JpaRepository<RentalItem, Long>, RentalItemQRepo {
     RentalItem findFirstByEquipmentItem_EquipmentItemIdAndActualReturnDateIsNull(Long equipmentItemId);
+
+    @Modifying
+    @Query("UPDATE RentalItem r SET r.status = 'OVERDUE' " +
+            "WHERE r.status = 'RENTED' AND r.actualReturnDate IS NULL AND r.endDate < CURRENT_DATE")
+    int markOverdueRentalItems();
 }
