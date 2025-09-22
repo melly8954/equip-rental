f03adc9f50d7ffbf08e8b9453e0d40b9946f97d2
haily <49448330+melly8954@users.noreply.github.com>
2025-09-22 18:43:49 +0900

    fix: 불필요한 코드 제거

--- src/test/java/com/equip/equiprental/equipment/service/EquipmentServiceImplTest.java
+++ src/test/java/com/equip/equiprental/equipment/service/EquipmentServiceImplTest.java
@@ -73,7 +73,6 @@ public class EquipmentServiceImplTest {
 
             List<MultipartFile> files = List.of(file);
             when(equipmentRepository.findByModel("MODEL_Y")).thenReturn(Optional.empty());
-            when(modelCodeGenerator.generate(request.getCategory(), request.getSubCategory())).thenReturn("MCODE456");
 
             when(equipmentRepository.save(any()))
                     .thenAnswer(invocation -> {
