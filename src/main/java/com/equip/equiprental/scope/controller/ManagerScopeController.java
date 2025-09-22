4c7117ce3c31a86047de791a35216707c44155fa
haily <49448330+melly8954@users.noreply.github.com>
2025-09-22 23:32:17 +0900

    feat: 매니저 스코프 변경 API 구현

--- src/main/java/com/equip/equiprental/scope/controller/ManagerScopeController.java
+++ src/main/java/com/equip/equiprental/scope/controller/ManagerScopeController.java
@@ -4,6 +4,7 @@ import com.equip.equiprental.auth.security.PrincipalDetails;
 import com.equip.equiprental.common.controller.ResponseController;
 import com.equip.equiprental.common.dto.ResponseDto;
 import com.equip.equiprental.common.interceptor.RequestTraceIdInterceptor;
+import com.equip.equiprental.scope.dto.ManagerScopeRequest;
 import com.equip.equiprental.scope.service.ManagerScopeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,19 +12,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/manager-scopes")
 public class ManagerScopeController implements ResponseController {
     private final ManagerScopeService managerScopeService;
 
-    @GetMapping("/manager-scopes/{equipmentId}")
+    @PostMapping("")
+    public ResponseEntity<ResponseDto<Void>> setManagerScope(@RequestBody ManagerScopeRequest dto) {
+        String traceId = RequestTraceIdInterceptor.getTraceId();
+        log.info("[매니저 스코프 변경 요청 API] TraceId={}", traceId);
+
+        managerScopeService.setScope(dto.getManagerId(), dto.getCategoryId());
+        return makeResponseEntity(traceId, HttpStatus.OK, null, "접근 범위가 업데이트되었습니다.", null);
+    }
+
+    @GetMapping("/{equipmentId}")
     @PreAuthorize("hasRole('ADMIN') or (hasRole('MANAGER'))")
     public ResponseEntity<ResponseDto<Boolean>> checkManagerScope(@PathVariable Long equipmentId,
                                                                   @AuthenticationPrincipal PrincipalDetails principal) {
