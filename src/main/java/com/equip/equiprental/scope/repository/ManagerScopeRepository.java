2552cfdd70699457a3ca869c644efe5e2c85ef2d
haily <49448330+melly8954@users.noreply.github.com>
2025-09-22 23:30:46 +0900

    feat: 매니저 스코프 관리 쿼리 메서드 추가

--- src/main/java/com/equip/equiprental/scope/repository/ManagerScopeRepository.java
+++ src/main/java/com/equip/equiprental/scope/repository/ManagerScopeRepository.java
@@ -1,8 +1,19 @@
 package com.equip.equiprental.scope.repository;
 
+import com.equip.equiprental.equipment.domain.Category;
+import com.equip.equiprental.member.domain.Member;
 import com.equip.equiprental.scope.domain.ManagerScope;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ManagerScopeRepository extends JpaRepository<ManagerScope, Integer> {
-    boolean existsByManagerIdAndCategory(Long managerId, String category);
+    boolean existsByManager_MemberIdAndCategory_CategoryId(Long managerId, Long categoryId);
+
+    @Query("SELECT m.category FROM ManagerScope m WHERE m.manager.memberId = :memberId")
+    Category findCategoryByManager(@Param("memberId") Long memberId);
+
+    Optional<ManagerScope> findByManager(Member manager);
 }
