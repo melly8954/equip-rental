1c308c5b6f12550f6463efa8b926fd6ef91dcae8
haily <49448330+melly8954@users.noreply.github.com>
2025-09-22 19:38:30 +0900

    feat: findByRental_RentalId 메서드 추가

--- src/main/java/com/equip/equiprental/rental/repository/RentalItemRepository.java
+++ src/main/java/com/equip/equiprental/rental/repository/RentalItemRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface RentalItemRepository extends JpaRepository<RentalItem, Long>, RentalItemQRepo {
     RentalItem findFirstByEquipmentItem_EquipmentItemIdAndActualReturnDateIsNull(Long equipmentItemId);
 
@@ -13,4 +15,6 @@ public interface RentalItemRepository extends JpaRepository<RentalItem, Long>, R
     @Query("UPDATE RentalItem r SET r.status = 'OVERDUE' " +
             "WHERE r.status = 'RENTED' AND r.actualReturnDate IS NULL AND r.endDate < CURRENT_DATE")
     int markOverdueRentalItems();
+
+    List<RentalItem> findByRental_RentalId(Long rentalId);
 }
